106b8d05fed547b08553caa2c10bc5ca58b66a24
Дмитрий Галашев <galashev1990@gmail.com>
2022-09-26 22:06:39 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,48 @@
+package Loading;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        openZip("D:/Games/savegames.zip", "D:/Games/savegames");
+        System.out.println(openProgress("D:/Games/savegames/save.dat"));
+
+    }
+
+    public static void openZip(String address, String location) {
+
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(address))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static GameProgress openProgress(String address) {
+        GameProgress gameProgress = null;
+
+        try (FileInputStream fis = new FileInputStream(address);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return gameProgress;
+    }
+}
